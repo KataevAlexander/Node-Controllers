4b98e29f0d95043c82ca5c38b7aa7921ddb6e052
alexanderkataev <kataev@realtimeboard.com>
2014-12-21 15:17:57 +0500

    commit

---
+++ backend/cluster/run.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+ 
+'use strict';
+ 
+var spawn = require('child_process').spawn;
+ 
+var args = [
+  '--harmony',
+  'index.js'
+];
+ 
+var opt = {
+  cwd: __dirname,
+  env: (function() {
+    process.env.NODE_PATH = __dirname + '/../'; // Enables require() calls relative to the cwd :)
+    return process.env;
+  }()),
+  stdio: [process.stdin, process.stdout, process.stderr]
+};
+ 
+var app = spawn(process.execPath, args, opt);
